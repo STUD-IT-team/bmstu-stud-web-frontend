732c62d5296b6f3fd0769f38eeb42723842c40eb
Сергей Тяпкин <Tyapkin2002@mail.ru>
2024-03-27 06:30:14 +0300

    Поправил страницу 404

--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -1,10 +1,13 @@
 <style lang="stylus" scoped>
+@require '../styles/components.styl'
+@require '../styles/buttons.styl'
 @require '../styles/constants.styl'
 @require '../styles/fonts.styl'
 
 .root-404
   width 100%
   padding 20px
+  margin-top 200px
   .form
     margin 20px auto
     max-width 600px
@@ -20,6 +23,9 @@
     .info
       font-medium()
       margin-bottom 15px
+    .link
+      font-large()
+      button()
 </style>
 
 <template>
@@ -27,7 +33,7 @@
     <div class="form">
       <div class="title">404 Страница не найдена</div>
       <div class="info">Запрошенной страницы не существует</div>
-      <router-link :to="{name: 'default'}"></router-link>
+      <router-link :to="{name: 'default'}" class="link">На главную</router-link>
     </div>
 
     <Footer></Footer>
