c69c81754520cf893b743252d5cf010cf34c72b1
just_rudy <sds22u306@student.bmstu.ru>
2024-07-01 17:20:24 +0300

    updating video-pic

--- src/views/PageNews.vue
+++ src/views/PageNews.vue
@@ -5,100 +5,19 @@
 @require '../styles/components.styl'
 @require '../styles/utils.styl'
 
-between-news-gap = 30px
-between-columns-gap = between-news-gap
-main-part-max-width = 1140px
-padding-sides = 20px
-
 .root-page-news
-  .main-container
-    padding 0 padding-sides
-    padding-top 50px
-    centered-margin()
-    max-width (main-part-max-width + padding-sides * 2)
-    .header
-      font-large-xx()
-      margin-bottom 20px
-    .navbar-organizations
-      margin-left-size = 'min(-%s, calc((100vw - %s) / -2))' % (padding-sides main-part-max-width)
-      padding-left-size = 'max(%s, calc((100vw - %s) / 2))' % (padding-sides main-part-max-width)
-      margin-left margin-left-size
-      padding 20px padding-left-size
-      width 100vw
-      border 1px solid white
-      border-left none
-      border-right none
-      margin-bottom 50px
-      .organization-button
-        padding 5px 3px
-        font-medium()
-        trans()
-        hover-effect-underline()
-    .news-blocks-section-container
-      display flex
-      gap between-columns-gap
-      @media ({mobile})
-        flex-direction column-reverse
-      .news-container
-        width 100%
-        display flex
-        flex-direction column
-        gap between-news-gap
-      .side-blocks-container
-        display flex
-        flex-direction column
-        gap between-news-gap
-        > *
-          @media ({desktop})
-            width 360px
+  position relative
+  .emotions-sector
+    position relative
+    img
+      width 100%
 </style>
 
 <template>
   <div class="root-page-news">
-    <Header></Header>
-
-    <main class="main-container">
-      <header class="header">Лента новостей</header>
-
-      <nav class="navbar-organizations">
-        <router-link to="#" class="organization-button">Все события</router-link>
-      </nav>
-
-      <section class="news-blocks-section-container">
-        <div class="news-container">
-          <New v-for="(oneNew, idx) in news"
-             :id="oneNew.id"
-             :title="oneNew.title"
-             :text="oneNew.description"
-             organization-name="Студнческий совет МГТУ&nbsp;им.&nbsp;Н.Э.&nbsp;Баумана"
-             :organization-logo="StudLogo"
-             :images-src="[Picture]"
-             :time-published="oneNew.created_at"
-             @delete="news.splice(idx, 1)"
-          ></New>
-        </div>
-        <div class="side-blocks-container">
-          <ListingBlock title="Поздравляем!"
-                        :text-rows="[
-                        'Поздравляем Антона Павленко с ДР!!!',
-                        'Поздравляем Антона Успенского со званием мисс',
-                        'А вас не бесит it отдел?',
-                        'Это студенческий совет МГТУ&nbsp;им.&nbsp;Н.Э.&nbsp;Баумана',
-                      ]">
-          </ListingBlock>
-          <ListingBlock title="Горячие новости"
-                        :text-rows="[
-                        'А вас не бесит it отдел?',
-                        'Меня лично бесит',
-                        'Можно выйти пж?',
-                        'Блин кнопку выхода не сделали :(',
-                        'Я запрещаю вам выходить из IT-отдела!',
-                      ]">
-          </ListingBlock>
-        </div>
-      </section>
-    </main>
-
+    <div class="emotions-sector">
+      <img src="/res/images/video.png" alt=""/>
+    </div>
     <Footer></Footer>
   </div>
 </template>
@@ -115,7 +34,7 @@ import Footer from "~/components/Footer.vue";
 
 
 export default {
-  components: {Footer, ListingBlock, New, Header},
+  components: { Footer, ListingBlock, New, Header },
 
   data() {
     return {
@@ -135,7 +54,7 @@ export default {
   methods: {
     async getNews() {
       this.loading = true;
-      const {data, ok, status} = await this.$api.getNews();
+      const { data, ok, status } = await this.$api.getNews();
       this.loading = false;
       if (!ok) {
         this.$popups.error(`Ошибка ${status}`, 'Не удалось получить новости')
