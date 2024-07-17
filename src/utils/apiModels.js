96f4a3d0e0b8da85e8053889c762d575d1656ee9
SodAcetate <sodium.acetate.82@gmail.com>
2024-07-17 12:00:15 +0300

    Поправил отображение новостей на главной

--- src/utils/apiModels.js
+++ src/utils/apiModels.js
@@ -125,7 +125,7 @@ export const Feed = {
     id: Number,
     title: String,
     description: String,
-    media: Media,
+    media: Media.media,
     updated_at: Date,
     created_at: Date,
     created_by: Number,
