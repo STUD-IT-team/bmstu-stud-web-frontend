a82d56527a0ffa0039691bb839577b0cf429bbd2
Antony Pavlenko <antonpawlenko@yandex.ru>
2024-11-27 21:45:23 +0300

    fixes

--- vite.config.js
+++ vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
       //   changeOrigin: true,
       // },
       '/api': {
-        target: `http://127.0.0.1:5000/bmstu-stud-web/`,
+        target: `http://127.0.0.1:5000`,
         secure: false,
         changeOrigin: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
